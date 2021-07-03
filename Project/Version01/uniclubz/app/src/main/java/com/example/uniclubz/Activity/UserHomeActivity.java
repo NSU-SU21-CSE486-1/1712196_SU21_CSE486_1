350b4f113d39fc689551b6fb6cd16bfd7bc9344a
istiaksaif <istiaksaif.nsu@gmail.com>
2021-07-03 12:23:40 +0600

    Registration #4 UniCluBz
    Registration UI some changes and fully functional also lottie and glide library added

---
+++ Project/Version01/uniclubz/app/src/main/java/com/example/uniclubz/Activity/UserHomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.uniclubz.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uniclubz.R;
+
+public class UserHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+    }
+}
