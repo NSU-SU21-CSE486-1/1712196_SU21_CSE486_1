79f20243a183ffdd5d392d9f53ff18e63c5021d7
istiaksaif <istiaksaif.nsu@gmail.com>
2021-08-19 18:22:35 +0600

    project02 #19 update firsttab

--- Theory/Assignment/Assignment02/Project02/app/src/main/java/com/istiaksaif/Project02/Fragment/FirstTabFragment.java
+++ Theory/Assignment/Assignment02/Project02/app/src/main/java/com/istiaksaif/Project02/Fragment/FirstTabFragment.java
@@ -1,17 +1,115 @@
 package com.istiaksaif.Project02.Fragment;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.istiaksaif.Project02.Activity.UniversityAffiliationActivity;
+import com.istiaksaif.Project02.MainActivity;
 import com.istiaksaif.Project02.R;
+
+import java.util.Calendar;
 
 public class FirstTabFragment extends Fragment {
 
+    public static TextInputEditText fullName,dateOfBirth,nid,bloodGroup;
+    private Button nextButton;
+    private String date;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        fullName = view.findViewById(R.id.name);
+        dateOfBirth = view.findViewById(R.id.dateofbirth);
+        dateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int nYear = calendar.get(Calendar.YEAR);
+                int nMonth = calendar.get(Calendar.MONTH);
+                int nDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(v.getContext(), android.R.style.Theme_Holo_Light_Dialog_MinWidth, datepickerListener, nYear, nMonth, nDay);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+        });
+        nid = view.findViewById(R.id.nid);
+        bloodGroup = view.findViewById(R.id.bloodgroup);
+//        nextButton = view.findViewById(R.id.next_button);
+//        nextButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Info();
+//            }
+//        });
+    }
+
+    private void Info() {
+        String FullName = fullName.getText().toString();
+        String DateOfBirth = dateOfBirth.getText().toString();
+        String NID = nid.getText().toString();
+        String BloodGroup = bloodGroup.getText().toString();
+
+        if (TextUtils.isEmpty(FullName)){
+            Toast.makeText(getActivity(), "please enter your Name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (TextUtils.isEmpty(DateOfBirth)){
+            Toast.makeText(getActivity(), "please enter DateOfBirth", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (TextUtils.isEmpty(NID)){
+            Toast.makeText(getActivity(), "please enter NID", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (NID.length()<13){
+            Toast.makeText(getActivity(), "Nid number minimum 13 and max 18 numbers", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (TextUtils.isEmpty(BloodGroup)){
+            Toast.makeText(getActivity(), "please enter bloodGroup", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(getActivity(), UniversityAffiliationActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("name",FullName);
+        bundle.putString("dateofbirth",DateOfBirth);
+        bundle.putString("nid",NID);
+        bundle.putString("bloodgroup",BloodGroup);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+    private DatePickerDialog.OnDateSetListener datepickerListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.YEAR,year);
+            calendar.set(Calendar.MONTH,month);
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+            month = month+1;
+            date = dayOfMonth+"/"+month+"/"+year;
+            dateOfBirth.setText(date);
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
