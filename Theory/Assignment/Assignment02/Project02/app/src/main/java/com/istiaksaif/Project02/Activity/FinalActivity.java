7cd30039bfb8ee48ccc9fb48652baff439d39540
istiaksaif <istiaksaif.nsu@gmail.com>
2021-08-19 18:31:28 +0600

    project02 #23 Final Result
    data save using file serialization

--- Theory/Assignment/Assignment02/Project02/app/src/main/java/com/istiaksaif/Project02/Activity/FinalActivity.java
+++ Theory/Assignment/Assignment02/Project02/app/src/main/java/com/istiaksaif/Project02/Activity/FinalActivity.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.istiaksaif.Project02.R;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FinalActivity extends AppCompatActivity {
 
-    private TextInputEditText fullName,dateOfBirth,nid,bloodGroup;
-    private TextView uniName,studentID,department,level,phone,email;
+    private TextView uniName,studentID,department,level,phone,email,personalInfo;
     private Toolbar toolbar;
     private Bundle b;
+
+    private String filename = "";
+    private  String filePath = "";
+    private String fileContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,38 +35,34 @@ public class FinalActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Profile");
-        fullName = findViewById(R.id.name);
-        dateOfBirth = findViewById(R.id.dateofbirth);
-        nid = findViewById(R.id.nid);
-        bloodGroup = findViewById(R.id.bloodgroup);
         uniName = findViewById(R.id.uniname);
         studentID = findViewById(R.id.id);
         department = findViewById(R.id.dep);
         level = findViewById(R.id.level);
         email = findViewById(R.id.email);
         phone = findViewById(R.id.phone);
+        personalInfo = findViewById(R.id.personalinfo);
 
-        if (b!=null){
-            String result = b.getString("name");
-            fullName.setText(result);
-            String result1 = b.getString("dateofbirth");
-            dateOfBirth.setText(result1);
-            String result3 = b.getString("bloodgroup");
-            bloodGroup.setText(result3);
-            String result2 = b.getString("nid");
-            nid.setText(result2);
-            String result4 = b.getString("uniname");
-            uniName.setText(result4);
-            String result5 = b.getString("department");
-            department.setText(result5);
-            String result6 = b.getString("id");
-            studentID.setText(result6);
-            String result7 = b.getString("studentLevel");
-            level.setText(result7);
-            String result8 = b.getString("email");
-            email.setText(result8);
-            String result9 = b.getString("phone");
-            phone.setText(result9);
+        filename = "userinfo.txt";
+        filePath = "project02Dir";
+        FileReader fileReader = null;
+        File myexFile = new File(getExternalFilesDir(filePath),filename);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            fileReader = new FileReader(myexFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+            while(line !=null){
+                stringBuilder.append(line).append('\n');
+                line = bufferedReader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            String fileContents = stringBuilder.toString();
+            personalInfo.setText(fileContents);
         }
     }
 }
--- Theory/Assignment/Assignment02/Project02/app/src/main/java/com/istiaksaif/Project02/Activity/ProfileActivity.java
+++ Theory/Assignment/Assignment02/Project02/app/src/main/java/com/istiaksaif/Project02/Activity/ProfileActivity.java
@@ -5,15 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputEditText;
 import com.istiaksaif.Project02.Adapter.TabViewPagerAdapter;
 import com.istiaksaif.Project02.Fragment.FirstTabFragment;
 import com.istiaksaif.Project02.Fragment.SecondTabFragment;
 import com.istiaksaif.Project02.Fragment.ThirdTabFragment;
+import com.istiaksaif.Project02.MainActivity;
 import com.istiaksaif.Project02.R;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.Permission;
 
 import static androidx.fragment.app.FragmentStatePagerAdapter.*;
 
@@ -21,11 +41,19 @@ public class ProfileActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager tabviewPager;
+    private String filename = "";
+    private String filePath = "";
+    private String fileContent;
+//    private TextInputEditText fullName,dateOfBirth,nid,bloodGroup;
+    private TextView textView,submitButton,swtichbtn;
+    private String fullName,dateOfBirth,nid,bloodGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        checkPermission();
 
         tabLayout = (TabLayout)findViewById(R.id.tab);
         tabviewPager = (ViewPager)findViewById(R.id.tabviewpager);
@@ -37,19 +65,94 @@ public class ProfileActivity extends AppCompatActivity {
         tabviewPager.setCurrentItem(0);
         tabLayout.setupWithViewPager(tabviewPager);
 
-        //transformPage between fragments
-//        tabviewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
-//            @Override
-//            public void transformPage(@NonNull View page, float position) {
-//                page.setTranslationX(page.getWidth()* -position);
-//                if(position <= -1 || position >= 1){
-//                    page.setAlpha(0);
-//                }else if(position==0){
-//                    page.setAlpha(1);
-//                }else {
-//                    page.setAlpha(1-Math.abs(position));
+//        fullName = findViewById(R.id.name);
+
+        submitButton = findViewById(R.id.submitbtn);
+        swtichbtn = findViewById(R.id.switchbtn);
+        swtichbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this,FinalActivity.class);
+                startActivity(intent);
+            }
+        });
+        //data save file
+        filename = "userinfo.txt";
+        filePath = "project02Dir";
+        if(!isExternalStorageRW()){
+            submitButton.setEnabled(false);
+        }
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fullName= FirstTabFragment.fullName.getText().toString().trim();
+                dateOfBirth= FirstTabFragment.dateOfBirth.getText().toString().trim();
+                nid= FirstTabFragment.nid.getText().toString().trim();
+                bloodGroup= FirstTabFragment.bloodGroup.getText().toString().trim();
+                fileContent ="Name : " +fullName+"\n"+"DOB: " +dateOfBirth+"\n"
+                +"NID : " +nid+"\n"+"Blood Group : " +bloodGroup;
+//                File file = new File(getExternalFilesDir(filePath),filename);
+//                if(!file.exists()){
+//                    try {
+//                        file.createNewFile();
+//                        FileWriter fileWriter = new FileWriter(file,true);
+//                        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//                        for(String string : fileContent){
+//                            fileContent += string+System.getProperty(System.lineSeparator());
+//                            bufferedWriter.write(fileContent);
+//                            bufferedWriter.close();
+//                        }
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
 //                }
-//            }
-//        });
+
+                if(!fileContent.equals("")){
+                    File myFile = new File(getExternalFilesDir(filePath),filename);
+                    FileOutputStream fileOutputStream = null;
+                    try {
+                        fileOutputStream = new FileOutputStream(myFile);
+                        fileOutputStream.write(fileContent.getBytes());
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(ProfileActivity.this,"done",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(ProfileActivity.this,FinalActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private boolean isExternalStorageRW() {
+        String storage = Environment.getExternalStorageState();
+        if (storage.equals(Environment.MEDIA_MOUNTED)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    private void checkPermission(){
+        if(Build.VERSION.SDK_INT>=23){
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!=
+                    PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(ProfileActivity.this,"Granted",Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(ProfileActivity.this,"Denied",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
