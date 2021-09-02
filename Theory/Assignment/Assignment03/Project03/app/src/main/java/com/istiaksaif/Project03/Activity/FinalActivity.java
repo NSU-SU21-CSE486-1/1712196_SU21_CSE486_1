d3f61aaf0f4ae0b515e8bfd9bdb5e506839b0967
istiaksaif <istiaksaif.nsu@gmail.com>
2021-09-02 12:56:15 +0600

    Assignment03 #29 rename project

---
+++ Theory/Assignment/Assignment03/Project03/app/src/main/java/com/istiaksaif/Project03/Activity/FinalActivity.java
@@ -0,0 +1,62 @@
+package com.istiaksaif.Project03.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.istiaksaif.Project03.R;
+
+public class FinalActivity extends AppCompatActivity {
+
+    private TextInputEditText fullName,dateOfBirth,nid,bloodGroup;
+    private TextView uniName,studentID,department,level,phone,email;
+    private Toolbar toolbar;
+    private Bundle b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final);
+
+        b = getIntent().getExtras();
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Profile");
+        fullName = findViewById(R.id.name);
+        dateOfBirth = findViewById(R.id.dateofbirth);
+        nid = findViewById(R.id.nid);
+        bloodGroup = findViewById(R.id.bloodgroup);
+        uniName = findViewById(R.id.uniname);
+        studentID = findViewById(R.id.id);
+        department = findViewById(R.id.dep);
+        level = findViewById(R.id.level);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+
+        if (b!=null){
+            String result = b.getString("name");
+            fullName.setText(result);
+            String result1 = b.getString("dateofbirth");
+            dateOfBirth.setText(result1);
+            String result3 = b.getString("bloodgroup");
+            bloodGroup.setText(result3);
+            String result2 = b.getString("nid");
+            nid.setText(result2);
+            String result4 = b.getString("uniname");
+            uniName.setText(result4);
+            String result5 = b.getString("department");
+            department.setText(result5);
+            String result6 = b.getString("id");
+            studentID.setText(result6);
+            String result7 = b.getString("studentLevel");
+            level.setText(result7);
+            String result8 = b.getString("email");
+            email.setText(result8);
+            String result9 = b.getString("phone");
+            phone.setText(result9);
+        }
+    }
+}
---
+++ Theory/Assignment/Assignment03/Project03/app/src/main/java/com/istiaksaif/Project03/Utils/optionUniName.java
@@ -0,0 +1,7 @@
+package com.istiaksaif.Project03.Utils;
+
+public class optionUniName {
+    public static final String[] optionUniName={"North South University","East West University","Dhaka University"
+            ,"BRAC University","AIUB","IUB","Shahjalal University of Science and Technology",
+            "Rajshahi University of Engineering & Technology","RU","Deffordial University"};
+}
