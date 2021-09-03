f49631523f7a3ba3642467f944dd30c4f54db918
istiaksaif <istiaksaif.nsu@gmail.com>
2021-09-03 22:28:38 +0600

    project03 #30 ActivityProfile Tab navigation implement
    added and update some gradle

---
+++ Theory/Assignment/Assignment03/Project03/app/src/main/java/com/istiaksaif/Project03/Activity/ProfileActivity.java
@@ -0,0 +1,82 @@
+package com.istiaksaif.Project03.Activity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.istiaksaif.Project03.Adapter.TabViewPagerAdapter;
+import com.istiaksaif.Project03.Fragment.FirstTabFragment;
+import com.istiaksaif.Project03.Fragment.SecondTabFragment;
+import com.istiaksaif.Project03.Fragment.ThirdTabFragment;
+import com.istiaksaif.Project03.R;
+import com.istiaksaif.Project03.Utils.User;
+import com.istiaksaif.Project03.roomdb.DatabaseClass;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager tabviewPager;
+    private TextView submitButton;
+    private ImageView swtichbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+        tabLayout = (TabLayout)findViewById(R.id.tab);
+        tabviewPager = (ViewPager)findViewById(R.id.tabviewpager);
+        TabViewPagerAdapter tabViewPagerAdapter = new TabViewPagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tabViewPagerAdapter.AddFragment(new FirstTabFragment(),"First Tab");
+        tabViewPagerAdapter.AddFragment(new SecondTabFragment(),"Second Tab");
+        tabViewPagerAdapter.AddFragment(new ThirdTabFragment(),"Third Tab");
+        tabviewPager.setAdapter(tabViewPagerAdapter);
+        tabviewPager.setCurrentItem(0);
+        tabLayout.setupWithViewPager(tabviewPager);
+
+        submitButton = findViewById(R.id.submitbtn);
+//        submitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                saveData();
+//            }
+//        });
+        swtichbtn = findViewById(R.id.switchbtn);
+        swtichbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, FinalActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void saveData() {
+
+        DatabaseClass db  = DatabaseClass.getDbInstance(this.getApplicationContext());
+
+        User model = new User();
+        model.getName();
+        model.getNID();
+        model.getBloodGroup();
+        model.getDOB();
+        db.userDao().insertUser(model);
+
+        Toast.makeText(this, "Data Successfully Saved", Toast.LENGTH_SHORT).show();
+    }
+}
